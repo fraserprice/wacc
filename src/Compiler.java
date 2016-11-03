2ec88f101472f9d32869113fbe8699deb8006eaf
Alexandru Toma <alexandru.toma15@imperial.ac.uk>
2016-11-03 16:03:16 +0000

    [AIT][MV][AMN] Added basic compiler structure

--- src/Compiler.java
+++ src/Compiler.java
@@ -1,4 +1,29 @@
+import antlr.WACCLexer;
+import antlr.WACCParser;
+import org.antlr.v4.parse.ANTLRLexer;
+import org.antlr.v4.runtime.ANTLRFileStream;
+import org.antlr.v4.runtime.CommonToken;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+import java.io.IOException;
+
 public class Compiler {
     public static void main(String[] args) {
+        ANTLRFileStream in = null;
+        try {
+            in = new ANTLRFileStream(args[0]);
+        } catch(IOException e) {
+            System.out.println(e.getMessage());
+            return;
+        }
+
+        WACCLexer lexer = new WACCLexer(in);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        WACCParser parser = new WACCParser(tokens);
+
+        ParseTree tree = parser.program();
+
+        System.out.println(tree);
     }
 }
