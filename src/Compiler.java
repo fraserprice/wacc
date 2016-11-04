e6f67714ab32da03001fe3d723b1712c2a4b0d52
Toma Alexandru <bartimaeus1073@gmail.com>
2016-11-04 13:33:26 +0000

    [AIT][MV][FP][AMN]
    Redirected errors from antlr to stderr and
    added exit code functionality.

--- src/Compiler.java
+++ src/Compiler.java
@@ -1,14 +1,16 @@
 import antlr.WACCLexer;
 import antlr.WACCParser;
+import antlr.WACCParserBaseVisitor;
 import org.antlr.v4.runtime.ANTLRFileStream;
 import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.tree.ParseTree;
-import org.antlr.v4.runtime.tree.gui.SystemFontMetrics;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.tree.*;
 
 import java.io.IOException;
 
 public class Compiler {
     public static final int EXIT_SUCCESS = 0;
+    public static final int EXIT_FILE_ERROR = 1;
     public static final int EXIT_SYNTAX_ERROR = 100;
     public static final int EXIT_SEMANTIC_ERROR = 200;
 
@@ -17,18 +19,25 @@ public class Compiler {
         try {
             in = new ANTLRFileStream(args[0]);
         } catch(IOException e) {
-            System.out.print(e.getMessage());
-            System.exit(EXIT_SYNTAX_ERROR);
+            System.err.println(e.getMessage());
+            System.exit(EXIT_FILE_ERROR);
         }
 
         WACCLexer lexer = new WACCLexer(in);
+        lexer.removeErrorListeners();
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         WACCParser parser = new WACCParser(tokens);
+        parser.removeErrorListeners();
 
+        WACCParserBaseVisitor<Integer> visitor = new WACCParserBaseVisitor<>();
 
-        ParseTree tree = parser.program();
+        visitor.visit(parser.program());
 
-        // System.out.println(tree);
+        if (parser.getNumberOfSyntaxErrors() > 0) {
+            System.err.println("#syntax_error#");
+            System.exit(EXIT_SYNTAX_ERROR);
+        }
+
         System.exit(EXIT_SUCCESS);
     }
 }
