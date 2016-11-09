377b37639682b163c7363dacb2039b42c9dc9cb1
mihaivanea <mihaivanea1@gmail.com>
2016-11-09 00:44:52 +0000

    [MV] implemented the function node

--- src/visitor/nodes/FunctionNode.java
+++ src/visitor/nodes/FunctionNode.java
@@ -1,21 +1,34 @@
 package visitor.nodes;
 
+import main.CompileTimeError;
 import org.antlr.v4.runtime.ParserRuleContext;
 import symobjects.SymbolTable;
 import visitor.Node;
+import visitor.nodes.stat.ReturnNode;
 import visitor.nodes.util.ParamNode;
 import visitor.nodes.type.TypeNode;
 
 import java.util.List;
 
-// TODO
 public class FunctionNode extends Node {
+
+    private TypeNode returnType;
+    private List<ParamNode> paramList;
+    private StatNode statNode;
+
     public FunctionNode(SymbolTable currentST, ParserRuleContext ctx, TypeNode typeNode,
                         List<ParamNode> paramNodeList, StatNode statNode) {
         super(currentST, ctx);
+        this.returnType = typeNode;
+        this.paramList = paramNodeList;
+        this.statNode = statNode;
+        check();
     }
 
     public void check() {
-
+        if (!(ctx.getChild(paramList.size()) instanceof ReturnNode)) {
+            addError(CompileTimeError.TYPE_MISMATCH_ERROR);
+            printSyntacticErrors();
+        }
     }
 }
