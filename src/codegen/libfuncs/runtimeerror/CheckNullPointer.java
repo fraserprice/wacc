30a129c29ce76a9c1c351b189a88a2f7186e7709
arthur30 <arthur.niculae96@gmail.com>
2016-11-19 23:45:42 +0000

    [AMN] Implemented CheckNullPointer

--- src/codegen/libfuncs/runtimeerror/CheckNullPointer.java
+++ src/codegen/libfuncs/runtimeerror/CheckNullPointer.java
@@ -3,7 +3,14 @@ package codegen.libfuncs.runtimeerror;
 import codegen.DataDir;
 import codegen.Instruction;
 import codegen.LibFunc;
+import codegen.instructions.BaseInstruction;
+import codegen.instructions.Ins;
+import codegen.operands.Immediate;
+import codegen.operands.LabelOp;
+import codegen.operands.Offset;
+import codegen.operands.Register;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CheckNullPointer extends LibFunc {
@@ -16,9 +23,30 @@ public class CheckNullPointer extends LibFunc {
         this.dataDir.put(ERROR_MESSAGE);
     }
 
+    /**
+     * check_null_pointer:
+     *		PUSH {lr}
+     *		CMP r0, #0
+     *		LDREQ r0, =msg_0
+     *		BLEQ p_throw_runtime_error
+     *		POP {pc}
+     * @return list of instructions needed for the check_null_pointer label
+     */
     @Override
     public List<Instruction> getInstructions() {
-        return null;
+        return new ArrayList<Instruction>() {{
+            add(new BaseInstruction(Ins.PUSH, Register.LR));
+            add(new BaseInstruction(Ins.CMP, Register.R0, new Offset(0)));
+            add(new BaseInstruction(Ins.LDREQ, Register.R0, new Immediate(dataDir.get(ERROR_MESSAGE))));
+            add(new BaseInstruction(Ins.BLEQ, new LabelOp(ThrowRuntimeError.FUNC_NAME)));
+            add(new BaseInstruction(Ins.POP, Register.PC));
+        }
+        };
+    }
+
+    @Override
+    public List<Class<? extends LibFunc>> getDependencies() {
+        return new ArrayList<Class<? extends LibFunc>>() {{ add(ThrowRuntimeError.class); }};
     }
 
     @Override
