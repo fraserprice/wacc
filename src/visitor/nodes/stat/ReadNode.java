ca301a133fffe4dea4c32e3b5364ce69c4c524f4
mihaivanea <mihaivanea1@gmail.com>
2016-11-21 19:13:36 +0000

    [MV] implemented the code generation for ReadNode

--- src/visitor/nodes/stat/ReadNode.java
+++ src/visitor/nodes/stat/ReadNode.java
@@ -3,12 +3,21 @@ package visitor.nodes.stat;
 import antlr.WACCParser;
 import codegen.CodeGenerator;
 import codegen.Instruction;
+import codegen.instructions.BaseInstruction;
 import codegen.instructions.Ins;
+import codegen.libfuncs.io.PrintInt;
+import codegen.libfuncs.io.Printable;
+import codegen.libfuncs.io.ReadChar;
+import codegen.libfuncs.io.ReadInt;
+import codegen.operands.LabelOp;
+import codegen.operands.Offset;
 import codegen.operands.Register;
 import main.CompileTimeError;
 import symobjects.SymbolTable;
+import symobjects.identifierobj.TypeObj;
 import symobjects.identifierobj.typeobj.scalarobj.CharObj;
 import symobjects.identifierobj.typeobj.scalarobj.IntObj;
+import visitor.nodes.ExprNode;
 import visitor.nodes.StatNode;
 import visitor.nodes.util.AssignLhsNode;
 
@@ -17,9 +26,13 @@ import java.util.List;
 
 public class ReadNode extends StatNode<WACCParser.ReadStatContext> {
 
+    private TypeObj typeObj;
+
     public ReadNode(SymbolTable currentST, WACCParser.ReadStatContext ctx,
                     AssignLhsNode assignLhsNode) {
         super(currentST, ctx);
+
+        this.typeObj = assignLhsNode.getType();
 
         if (assignLhsNode.hasErrors()) {
             setError();
@@ -41,5 +54,17 @@ public class ReadNode extends StatNode<WACCParser.ReadStatContext> {
     public List<Instruction> generateInstructions(CodeGenerator codeGenRef, List<Register> availableRegisters) {
         List<Instruction> instructions = new ArrayList<>();
 
+        LabelOp labelOp = new LabelOp("0");
+
+        if (typeObj instanceof IntObj) {
+            labelOp = new LabelOp(ReadInt.FUNC_NAME_READ_INT);
+        } else if (typeObj instanceof CharObj) {
+            labelOp = new LabelOp(ReadChar.FUNC_NAME_READ_CHAR);
+        }
+
+        instructions.add(new BaseInstruction(Ins.MOV, Register
+                .R0, availableRegisters.get(0)));
+        instructions.add(new BaseInstruction(Ins.BL, labelOp));
+        return instructions;
     }
 }
