a281a54d0178feec72f01dc6a7cd33e994827282
Fraser Price <fraser@dyn1184-234.wlan.ic.ac.uk>
2016-11-21 15:30:11 +0000

    [FP] Completed literal expr code gen. Also added add/get message methods in codegen

--- src/visitor/nodes/expr/literal/BoolNode.java
+++ src/visitor/nodes/expr/literal/BoolNode.java
@@ -1,14 +1,32 @@
 package visitor.nodes.expr.literal;
 
 import antlr.WACCParser;
+import codegen.CodeGenerator;
+import codegen.Instruction;
+import codegen.instructions.BaseInstruction;
+import codegen.instructions.Ins;
+import codegen.operands.Immediate;
+import codegen.operands.Register;
 import symobjects.SymbolTable;
 import symobjects.identifierobj.typeobj.scalarobj.BoolObj;
 import visitor.nodes.expr.LiteralNode;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class BoolNode extends LiteralNode<WACCParser.BoolLiteralContext> {
+
     public BoolNode(SymbolTable currentST, WACCParser.BoolLiteralContext ctx) {
         super(currentST, ctx);
 
-        type = currentST.lookupAll("bool", BoolObj.class);
+        this.type = currentST.lookupAll("bool", BoolObj.class);
+        this.value = ctx.BOOL_LITERAL().getText().equals("true") ? "1" : "0";
+    }
+
+    @Override
+    public List<Instruction> generateInstructions(CodeGenerator codeGenRef, List<Register> availableRegisters) {
+        return new LinkedList<Instruction>() {{
+            add(new BaseInstruction(Ins.MOV, availableRegisters.get(0), new Immediate(value)));
+        }};
     }
 }
