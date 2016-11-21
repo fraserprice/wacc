531b0bd385ec48836bc450f314a7584fd84df69d
Fraser Price <fraser@dyn1184-234.wlan.ic.ac.uk>
2016-11-21 19:53:49 +0000

    [FP] Master merge

--- src/codegen/instructions/Ins.java
+++ src/codegen/instructions/Ins.java
@@ -2,6 +2,6 @@ package codegen.instructions;
 
 // All the Instructions defined for WACC
 public enum Ins {
-    ADD, ADDS, AND, ASR, BEQ, BL, BLCS, BLEQ, BLLT, BLNE, BLVS, CMP, EOR, LDR, LDRCS, LDREQ, LDRLT, LDRNE, LDRSB, MOV,
+    ADD, ADDS, AND, ASR, B, BEQ, BL, BLCS, BLEQ, BLLT, BLNE, BLVS, CMP, EOR, LDR, LDRCS, LDREQ, LDRLT, LDRNE, LDRSB, MOV,
     MOVEQ, MOVGE, MOVGT, MOVLE, MOVLT, MOVNE, ORR, POP, PUSH, RSBS, SMULL, STR, STRB, SUB, SUBS;
 }
