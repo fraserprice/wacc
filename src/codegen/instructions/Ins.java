cde0438eb7e4de374e0d1c010e1a8005c7c060d2
Fraser Price <fraser@dyn1184-234.wlan.ic.ac.uk>
2016-11-21 16:46:41 +0000

    [FP] Added codegen for BinOp along with instructions and ShiftOp class

--- src/codegen/instructions/Ins.java
+++ src/codegen/instructions/Ins.java
@@ -2,6 +2,6 @@ package codegen.instructions;
 
 // All the Instructions defined for WACC
 public enum Ins {
-    ADD, ADDS, BEQ, BL, BLCS, BLEQ, BLLT, CMP, LDR, LDRCS, LDREQ, LDRLT
-    , LDRNE, LDRSB, MOV, MOVEQ, MOVNE, POP, PUSH, STR, STRB, SUB;
+    ADD, ADDS, AND, ASR, BEQ, BL, BLCS, BLEQ, BLLT, BLNE, BLVS, CMP, LDR, LDRCS, LDREQ, LDRLT, LDRNE, LDRSB, MOV,
+    MOVEQ, MOVGE, MOVGT, MOVLE, MOVLT, MOVNE, ORR, POP, PUSH, SMULL, STR, STRB, SUB, SUBS;
 }
