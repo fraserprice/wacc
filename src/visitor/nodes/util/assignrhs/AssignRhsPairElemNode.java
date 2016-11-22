bb79fa0b837277553602bad7896bcdca015f0837
Toma Alexandru <bartimaeus1073@gmail.com>
2016-11-22 14:22:02 +0000

    [AIT] Implemented rhs

--- src/visitor/nodes/util/assignrhs/AssignRhsPairElemNode.java
+++ src/visitor/nodes/util/assignrhs/AssignRhsPairElemNode.java
@@ -1,15 +1,25 @@
 package visitor.nodes.util.assignrhs;
 
 import antlr.WACCParser;
+import codegen.CodeGenerator;
+import codegen.Instruction;
+import codegen.operands.Register;
 import symobjects.SymbolTable;
 import visitor.nodes.util.AssignRhsNode;
 import visitor.nodes.util.PairElemNode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AssignRhsPairElemNode extends AssignRhsNode<WACCParser.AssignRhsPairElemContext> {
+    private PairElemNode pNode;
+
     // assignRhs: pairElem
     public AssignRhsPairElemNode(SymbolTable currentST, WACCParser.AssignRhsPairElemContext
             ctx, PairElemNode pairElem) {
         super(currentST, ctx);
+
+        this.pNode = pairElem;
 
         if (pairElem.hasErrors()) {
             setError();
@@ -19,4 +29,8 @@ public class AssignRhsPairElemNode extends AssignRhsNode<WACCParser.AssignRhsPai
         this.type = pairElem.getType();
     }
 
+    @Override
+    public List<Instruction> generateInstructions(CodeGenerator codeGenRef, List<Register> availableRegisters) {
+        return pNode.generateInstructions(codeGenRef, availableRegisters);
+    }
 }
