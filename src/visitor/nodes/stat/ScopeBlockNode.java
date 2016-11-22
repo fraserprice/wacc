4d8847eeecb62419666283739bcaa20e4d7e7d0a
Fraser Price <fraser@dyn1185-230.wlan.ic.ac.uk>
2016-11-22 19:11:22 +0000

    [FP][AMN] Implemented Codegen for ScopeBlock

--- src/visitor/nodes/stat/ScopeBlockNode.java
+++ src/visitor/nodes/stat/ScopeBlockNode.java
@@ -1,8 +1,14 @@
 package visitor.nodes.stat;
 
 import antlr.WACCParser;
+import codegen.CodeGenerator;
+import codegen.Instruction;
+import codegen.operands.Register;
 import symobjects.SymbolTable;
 import visitor.nodes.StatNode;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class ScopeBlockNode extends StatNode<WACCParser.ScopeBlockStatContext> {
     private StatNode body;
@@ -21,5 +27,15 @@ public class ScopeBlockNode extends StatNode<WACCParser.ScopeBlockStatContext> {
 
     public StatNode getBody() {
         return body;
+    }
+
+    @Override
+    public List<Instruction> generateInstructions(CodeGenerator codeGenRef, List<Register> availableRegisters) {
+        List<Instruction> instructions = new LinkedList<>();
+
+        List<Instruction> bodyInstr = body.generateInstructions(codeGenRef, availableRegisters);
+        instructions.addAll(CodeGenerator.makeSpaceOnStack(currentST, bodyInstr));
+
+        return instructions;
     }
 }
