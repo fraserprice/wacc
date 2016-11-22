daeb92fc43b806efed39b786d7c1d2794ea4a5e6
Fraser Price <fraser@dyn1185-230.wlan.ic.ac.uk>
2016-11-22 21:23:39 +0000

    [FP] Implemented CodeGen for AssignLhsIdentNode

--- src/visitor/nodes/util/assignlhs/AssignLhsIdentNode.java
+++ src/visitor/nodes/util/assignlhs/AssignLhsIdentNode.java
@@ -1,10 +1,20 @@
 package visitor.nodes.util.assignlhs;
 
 import antlr.WACCParser;
+import codegen.CodeGenerator;
+import codegen.Instruction;
+import codegen.instructions.BaseInstruction;
+import codegen.instructions.Ins;
+import codegen.operands.Offset;
+import codegen.operands.Register;
+import codegen.operands.StackLocation;
 import main.CompileTimeError;
 import symobjects.SymbolTable;
 import symobjects.identifierobj.VariableObj;
 import visitor.nodes.util.AssignLhsNode;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class AssignLhsIdentNode extends AssignLhsNode<WACCParser.AssignLhsIdentContext> {
     // assignLhs: IDENT
@@ -26,5 +36,21 @@ public class AssignLhsIdentNode extends AssignLhsNode<WACCParser.AssignLhsIdentC
         this.ident = ctx.IDENT().getText();
     }
 
+    @Override
+    public List<Instruction> generateInstructions(CodeGenerator codeGenRef, List<Register> availableRegisters) {
+        int offset = currentST.lookupOffset(ident);
 
+        List<Instruction> instructions = new LinkedList<Instruction>() {{
+            if(offset == 0) {
+                add(new BaseInstruction(Ins.getLdrInstruction(type), availableRegisters.get(0),
+                        new StackLocation(Register.SP)));
+
+            } else {
+                add(new BaseInstruction(Ins.getLdrInstruction(type), availableRegisters.get(0),
+                        new StackLocation(Register.SP, new Offset(offset))));
+            }
+        }};
+
+        return instructions;
+    }
 }
